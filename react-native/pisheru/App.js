2fb5e47b0254801def2671e3c21fd60095543b29
Majid <majid.mjh@gmail.com>
2024-03-17 16:25:50 +0100

    change client ids and put on .env file

--- react-native/pisheru/App.js
+++ react-native/pisheru/App.js
@@ -1,63 +1,85 @@
-import * as React from 'react';
-import * as WebBrowser from 'expo-web-browser';
-import * as Google from 'expo-auth-session/providers/google';
-import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
+import * as React from "react";
+import * as WebBrowser from "expo-web-browser";
+import * as Google from "expo-auth-session/providers/google";
+import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
+
+import { CLIENT_WEB_ID, CLIENT_IOS_ID } from "@env";
 
 WebBrowser.maybeCompleteAuthSession();
 
 export default function App() {
   const [accessToken, setAccessToken] = React.useState(null);
   const [user, setUser] = React.useState(null);
+
   const [request, response, promptAsync] = Google.useIdTokenAuthRequest({
-    clientId: "843667796462-b1nrunqsdcqng41e3uaobmb42ifgf802.apps.googleusercontent.com",
-    iosClientId: "843667796462-l4dgkto14n2h1e3h4225tmarblh5tf6v.apps.googleusercontent.com",
+    clientId: String(CLIENT_WEB_ID),
+    iosClientId: String(CLIENT_IOS_ID),
   });
 
+
   React.useEffect(() => {
-    if(response?.type === "success") {
-      console.log(response.authentication)
+    if (response?.type === "success") {
       setAccessToken(response.authentication.accessToken);
       accessToken && fetchUserInfo();
     }
-  }, [response, accessToken])
+  }, [response, accessToken]);
 
   async function fetchUserInfo() {
     let response = await fetch("https://www.googleapis.com/userinfo/v2/me", {
-      headers: { Authorization: `Bearer ${accessToken}` }
+      headers: { Authorization: `Bearer ${accessToken}` },
     });
     const useInfo = await response.json();
     setUser(useInfo);
   }
 
   const ShowUserInfo = () => {
-    if(user) {
-      return(
-        <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-          <Text style={{fontSize: 35, fontWeight: 'bold', marginBottom: 20}}>Welcome</Text>
-          <Image source={{uri: user.picture}} style={{width: 100, height: 100, borderRadius: 50}} />
-          <Text style={{fontSize: 20, fontWeight: 'bold'}}>{user.name}</Text>
+    if (user) {
+      return (
+        <View
+          style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
+        >
+          <Text style={{ fontSize: 35, fontWeight: "bold", marginBottom: 20 }}>
+            Welcome
+          </Text>
+          <Image
+            source={{ uri: user.picture }}
+            style={{ width: 100, height: 100, borderRadius: 50 }}
+          />
+          <Text style={{ fontSize: 20, fontWeight: "bold" }}>{user.name}</Text>
         </View>
-      )
+      );
     }
-  }  
+  };
 
   return (
     <View style={styles.container}>
       {user && <ShowUserInfo />}
-      {user === null &&
-          <>
-          <Text style={{fontSize: 35, fontWeight: 'bold'}}>Welcome</Text>
-          <Text style={{fontSize: 25, fontWeight: 'bold', marginBottom: 20, color: 'gray'}}>Please login</Text>
-        <TouchableOpacity
-          disabled={!request}
-          onPress={() => {
-            promptAsync();
-            }} 
-        >
-          <Image source={require("./btn.png")} style={{width: 300, height: 40}} />
-        </TouchableOpacity>
+      {user === null && (
+        <>
+          <Text style={{ fontSize: 35, fontWeight: "bold" }}>Welcome</Text>
+          <Text
+            style={{
+              fontSize: 25,
+              fontWeight: "bold",
+              marginBottom: 20,
+              color: "gray",
+            }}
+          >
+            Please login
+          </Text>
+          <TouchableOpacity
+            disabled={!request}
+            onPress={() => {
+              promptAsync();
+            }}
+          >
+            <Image
+              source={require("./btn.png")}
+              style={{ width: 300, height: 40 }}
+            />
+          </TouchableOpacity>
         </>
-      }
+      )}
     </View>
   );
 }
@@ -65,8 +87,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
