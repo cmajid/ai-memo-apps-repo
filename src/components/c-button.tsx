8baa4e59e140182784db5484e0ede336dde81408
Majid <majid.mjh@gmail.com>
2024-03-18 22:09:47 +0100

    clean up login

--- src/components/c-button.tsx
+++ src/components/c-button.tsx
@@ -1,7 +1,6 @@
-import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import { Text, StyleSheet, TouchableOpacity } from "react-native";
 import Icon from "react-native-vector-icons/FontAwesome";
 import { LinearGradient } from "expo-linear-gradient";
-import { ButtonProps } from "react-native/Libraries/Components/Button";
 
 interface props {
   onPress: any;
@@ -14,11 +13,7 @@ export function CButton({ onPress, icon, title, colors }: props) {
   const bgColor = colors ?? ["#841584"];
   return (
     <TouchableOpacity style={styles.container} onPress={onPress}>
-      <LinearGradient
-        // Button Linear Gradient
-        colors={bgColor}
-        style={styles.button}
-      >
+      <LinearGradient colors={bgColor} style={styles.button}>
         <Icon name={icon} color="#fff" size={15} style={styles.icon}></Icon>
         <Text style={styles.text}>{title}</Text>
       </LinearGradient>
@@ -36,9 +31,8 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     borderRadius: 8,
 
-
-    borderColor: '#fff', 
-    borderWidth: 2, 
+    borderColor: "#fff",
+    borderWidth: 2,
   },
   icon: {
     marginRight: 10,
